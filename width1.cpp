2d5c93d21dfb0e071df1ec8933f1cc3833f49e85
WePlayFleX <alex.kamai@ya.ru>
2021-11-30 00:14:15 +0300

    Добавьте файлы проекта.

---
+++ width1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long pop1 = 4789426, pop2 = 274124, pop3 = 9761;
+	cout << "Town " << "Population " << endl
+		<< "Moscow " << pop1 << endl
+		<< "Kirov " << pop2 << endl
+		<< "Uhrymovka " << pop3 << endl;
+
+	system("pause");
+	return 0;
+}
